e69ce5f213be4ce343bef59f625e75db47b5e8d3
taha-cis <itstudentis3@gmail.com>
2022-04-26 18:14:06 +0300

    Fixing The Bugs In Add Patient Page

--- src/clinic/Database.java
+++ src/clinic/Database.java
@@ -1,6 +1,5 @@
 package clinic;
 
-import com.mysql.cj.xdevapi.PreparableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,10 +8,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Database {
-
     private static final String URL = "jdbc:mysql://localhost:3306/my_clinic"; // حدد اسم  القاعدة حسب الموجودة عندك
     private static final String USER = "root";
-    private static final String PASSWORD = "34634877"; // حدد كلمة المرور حسب الموجودة عندك
+    private static final String PASSWORD = "1+2*$#"; // 34634877 حدد كلمة المرور حسب الموجودة عندك
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
 
@@ -36,22 +34,25 @@ public class Database {
         // اضف استعلام استرجاع معلومات كل المرض
         return statement.executeQuery("SELECT اكمل الاستعلام ");
     }
-    
-    public static void addPatient(String pName, String phoneNumber, String pAddress, String pStatus,String notes) throws ClassNotFoundException, SQLException{
+
+    // دالة اضافة بيانات المرضى
+    public static int addPatient(String pName, String phoneNumber, String pAddress, String pStatus, String notes) throws ClassNotFoundException, SQLException {
+
         String query = "INSERT INTO adding_pateints(p_name,p_phone_number, p_address, p_status, notes) "
                 + "VALUES(?,?,?,?,?)";
-     
-         PreparedStatement pstmt = getConnection().prepareStatement(query);
-         pstmt.setString(1, pName);
-         pstmt.setString(2, phoneNumber);
-         pstmt.setString(3, pAddress);
-         pstmt.setString(4, pStatus);
-         pstmt.setString(5, notes);
-         pstmt.execute();
+
+        PreparedStatement pstmt = getConnection().prepareStatement(query);
+        pstmt.setString(1, pName);
+        pstmt.setString(2, phoneNumber);
+        pstmt.setString(3, pAddress);
+        pstmt.setString(4, pStatus);
+        pstmt.setString(5, notes);
+
+        return pstmt.executeUpdate();
     }
-    
+
     // The Method For Test Only
-      public static void updatePatient(String name, String address) throws ClassNotFoundException, SQLException{
+    public static void updatePatient(String name, String address) throws ClassNotFoundException, SQLException {
         // code gose here  
         // hello
     }
